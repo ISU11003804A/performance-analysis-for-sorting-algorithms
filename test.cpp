76f10a345a99fd30caa43f806f79bd9658b224fb
Rainbow0903 <114150382+ISU11003804A@users.noreply.github.com>
2022-09-25 04:24:16 +0800

    0

--- test.cpp
+++ test.cpp
@@ -1,28 +1,12 @@
 #include<iostream>
 #include<random>
-#include<iomanip>
+#include<ctime>
 #include<string>
 using namespace std;
-
-string random_string()
-{
-     string str("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
-
-     random_device rd;
-     mt19937 generator(rd());
-
-     shuffle(str.begin(), str.end(), generator);
-
-     return str.substr(0, 32);    // assumes 32 < number of characters in str         
-}
 
 int main()
 {
     
-    random_device rd;
-	mt19937 gen(rd());
-	uniform_real_distribution<double>dist(1,10000);
-
     for(int i=0;i<10;i++)
     {
         cout << random_string() << " " << endl;
