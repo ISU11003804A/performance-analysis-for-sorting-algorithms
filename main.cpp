efcd0cceaeb439791cc2ed705c78dadf8889f29f
Rainbow0903 <114150382+ISU11003804A@users.noreply.github.com>
2022-09-25 16:35:49 +0800

    0

--- main.cpp
+++ main.cpp
@@ -10,27 +10,6 @@ int main()
 {
     int number[11]={10,20,30,40,50,60,100000,200000,300000,400000,500000};
     
-    // for(int i=0;i<11;i++)
-    // {
-    //     insertion_sort_int(number[i]);
-    // }
-    // for(int i=0;i<11;i++)
-    // {
-    //     insertion_sort_long(i);
-    // }
-    // for(int i=0;i<11;i++)
-    // {
-    //     insertion_sort_float(i);
-    // }
-    // for(int i=0;i<11;i++)
-    // {
-    //     insertion_sort_double(i);
-    // }
-    // for(int i=0;i<11;i++)
-    // {
-    //     insertion_sort_string(i);
-    // }
-    
     merge_sort_int(1000);
     merge_sort_long(1000);
     merge_sort_float(1000);
