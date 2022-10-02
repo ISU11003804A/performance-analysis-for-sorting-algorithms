c6b64660e63ee2889639d5f96e0afc643d6b0df0
Rainbow0903 <114150382+ISU11003804A@users.noreply.github.com>
2022-10-03 01:01:12 +0800

    0

--- std array/main.cpp
+++ std array/main.cpp
@@ -8,8 +8,116 @@ using namespace std;
 
 int main()
 {
-    //insertion_sort_int(10);
-    merge_sort_string(1000);
+    int data_value[11]={10,20,30,40,50,60,100000,200000,300000,400000,500000};
+    
+    
+    // insertion_sort
+    for(int i=0;i<11;i++)
+    {
+        insertion_sort_int(data_value[i]);
+    }
+    
+    for(int i=0;i<11;i++)
+    {
+        insertion_sort_long(data_value[i]);
+    }
 
+    for(int i=0;i<11;i++)
+    {
+        insertion_sort_float(data_value[i]);
+    }
+    
+    for(int i=0;i<11;i++)
+    {
+        insertion_sort_double(data_value[i]);
+    }
+    
+    for(int i=0;i<11;i++)
+    {
+        insertion_sort_string(data_value[i]);
+    }
+
+
+    // selection_sort
+    for(int i=0;i<11;i++)
+    {
+        selection_sort_int(data_value[i]);
+    }
+
+    for(int i=0;i<11;i++)
+    {
+        selection_sort_long(data_value[i]);
+    }
+
+    for(int i=0;i<11;i++)
+    {
+        selection_sort_float(data_value[i]);
+    }
+
+    for(int i=0;i<11;i++)
+    {
+        selection_sort_double(data_value[i]);
+    }
+
+    for(int i=0;i<11;i++)
+    {
+        selection_sort_string(data_value[i]);
+    }
+
+
+    // bubble_sort
+    for(int i=0;i<11;i++)
+    {
+        bubble_sort_int(data_value[i]);
+    }
+
+    for(int i=0;i<11;i++)
+    {
+        bubble_sort_long(data_value[i]);
+    }
+
+    for(int i=0;i<11;i++)
+    {
+        bubble_sort_float(data_value[i]);
+    }
+
+    for(int i=0;i<11;i++)
+    {
+        bubble_sort_double(data_value[i]);
+    }
+
+    for(int i=0;i<11;i++)
+    {
+        bubble_sort_string(data_value[i]);
+    }
+
+
+    // merge_sort
+    for(int i=0;i<11;i++)
+    {
+        merge_sort_int(data_value[i]);
+    }
+    
+    for(int i=0;i<11;i++)
+    {
+        merge_sort_long(data_value[i]);
+    }
+
+    for(int i=0;i<11;i++)
+    {
+        merge_sort_float(data_value[i]);
+    }
+
+    for(int i=0;i<11;i++)
+    {
+        merge_sort_double(data_value[i]);
+    }
+
+    for(int i=0;i<11;i++)
+    {
+        merge_sort_string(data_value[i]);
+    }
+
+    system("pause");
     return 0;
 }
