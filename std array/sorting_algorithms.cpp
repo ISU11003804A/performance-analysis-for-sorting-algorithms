911122114bf76cd25ae8ea6a9621f2dcef9b5792
ISU11003804A <114150382+ISU11003804A@users.noreply.github.com>
2022-11-29 23:11:38 +0800

    Update sorting_algorithms.cpp

--- std array/sorting_algorithms.cpp
+++ std array/sorting_algorithms.cpp
@@ -6,29 +6,29 @@ using namespace std;
 template <class T>
 void PrintArray(T &arr, int size)
 {
-    for (int i=0;i<size;i++)
+	for (int i=0;i<size;i++)
 	{
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+        	cout << arr[i] << " ";
+    	}
+    	cout << endl;
 }
 
 // insertion_sort
 template <class T>
 void InsertionSort(T &arr, int size) 
 {
-    int i,j;
-    for(i=1;i<size;i++)
-    {
-        j=i;
-        auto temp=arr[i];
-        while(j>0&&temp<arr[j-1])
-        {
-           arr[j]=arr[j-1];
-           j--;
-        }
-        arr[j]=temp;
-    }
+	int i,j;
+	for(i=1;i<size;i++)
+    	{
+        	j=i;
+        	auto temp=arr[i];
+        	while(j>0&&temp<arr[j-1])
+        	{
+           		arr[j]=arr[j-1];
+           		j--;
+        	}
+        	arr[j]=temp;
+    	}
 }
 
 // selection_sort
@@ -36,17 +36,17 @@ template <class T>
 void SelectionSort(T &arr,int size)
 {
 	for(int i=0;i<size;i++)
-    {
+    	{
 		for(int j=i+1;j<size;j++)
-        {
-            if(arr[i]>arr[j])
-            {
+        	{
+            		if(arr[i]>arr[j])
+            		{
 				auto temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
-            }
-        }
-    }
+                		arr[i]=arr[j];
+                		arr[j]=temp;
+            		}
+        	}
+    	}
 }
 
 // bubble_sort
@@ -55,15 +55,15 @@ void BubbleSort(T &arr,int size)
 {
 	for(int i=size-1;i>0;i--)
 	{
-    	for(int j=0;j<=i-1;j++)
-    	{
-        	if(arr[j]>arr[j+1])
-        	{
+    		for(int j=0;j<=i-1;j++)
+    		{
+        		if(arr[j]>arr[j+1])
+        		{
 				auto temp=arr[j];
-            	arr[j]=arr[j+1];
-            	arr[j+1]=temp;
-        	}
-    	}
+            			arr[j]=arr[j+1];
+            			arr[j+1]=temp;
+        		}
+    		}
 	}
 }
 
@@ -106,8 +106,8 @@ void MergeSort(T &arr,int size,U &typ)
 		return;
 	}
     
-    array<U,1000000> L;
-    array<U,1000000> R;
+    	array<U,1000000> L;
+    	array<U,1000000> R;
 	
 	int mid=size/2;
 
